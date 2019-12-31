77e2fd8062b5b9023d91d2e1cedd4a0a59f5ae54
Daniel Park <pshappyyou@gmail.com>
2019-12-31 11:47:33 +1100

    Adding TSM status and open CMD

--- StartService.bat
+++ StartService.bat
@@ -9,15 +9,15 @@ ECHO PRESS 1, 2 OR 3 to select your task, or 4 to EXIT.
 ECHO ...............................................
 ECHO.
 ECHO 1 - Start Service
-ECHO 2 - TSM restart
-ECHO 3
+ECHO 2 - TSM status
+ECHO 3 - TSM restart
 ECHO 4 - EXIT
 ECHO.
 
 SET /P M=Type 1, 2, 3, or 4 then press ENTER:
 IF %M%==1 GOTO SERVICE
-IF %M%==2 GOTO TSM
-IF %M%==3 GOTO CMD
+IF %M%==2 GOTO TSMCHECK
+IF %M%==3 GOTO TSM
 IF %M%==4 GOTO EOF
 
 
@@ -45,10 +45,14 @@ PAUSE
 GOTO MENU
 REM tsm restart -u dpark -p Janu@123
 
-:TSM
-ECHO tsm restart -u dpark -p Janu@123
-tsm restart -u dpark -p Janu@123
+:TSMCHECK
+ECHO tsm status -v -u dpark -p Janu@123
+start cmd /k tsm status -v -u dpark -p Janu@123
 PAUSE
 GOTO MENU
 
-	
+:TSM
+ECHO tsm restart -u dpark -p Janu@123
+start /k tsm restart -u dpark -p Janu@123
+PAUSE
+GOTO MENU
